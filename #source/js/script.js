7f2a4f8229032a02362cd3e97034cc02723c549e
irzi <genikm@ukr.net>
2022-11-14 15:50:00 +0200

    Added support of ie 11

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,18 +1,18 @@
 'use strict'
-
 const buttonMore = document.querySelector('.delivery__paragraph_more');
 const moreParagraph = document.querySelectorAll('._more');
 
 if (innerWidth <= 480) {
-	buttonMore.addEventListener('click', () => {
-		moreParagraph.forEach(item => {
+	buttonMore.addEventListener('click', function() {
+		for (let i = 0; i < moreParagraph.length; i++) {
+			const item = moreParagraph[i];
 			if (item.style.display === 'block' ) {
 				item.style.display = 'none';
 			}else {
 				item.style.display = 'block';
 				buttonMore.style.display = 'none';
 			}
-		})
+		}
 	})
 }else {
 	buttonMore.style.display = 'none';
